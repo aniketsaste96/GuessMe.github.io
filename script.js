a33bc29ffaa5a939f394ba7c89e6c747600de9d2
Aniket <aniketsaste96@gmail.com>
2021-11-18 12:06:14 +0530

    guess

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
+var y = Math.floor(Math.random() * 100 + 1);
+console.log(y);
+
+
+
 
 function GuessMe() {
+
+
     var a = document.getElementById("number").value;
-    if (a < 86) {
-        alert("Enter the larger value")
-    } else if (a > 86) {
-        alert("Enter the smaller value")
+
+    if (a < y) {
+        alert("Opss!!Enter the larger value ")
+    } else if (a > y) {
+        alert("Opss!!Enter the smaller value")
     } else { alert("Congratulations You caught me!!") }
 }
 
